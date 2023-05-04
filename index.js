8d3500a0f4c209030622285537bc8a028c8d6ef5
jahangir2k04 <jahangir2k04gmail.com>
2023-05-04 12:06:05 +0600

    all chefs data sended toclient site

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 
+const chefData = require('./data/chefs.json');
+
+app.use(cors());
+
 app.get('/', (req, res) => {
-    res.send('RannaGhor is running')
+    res.send(chefData);
 })
 
 app.listen(port, () => {
